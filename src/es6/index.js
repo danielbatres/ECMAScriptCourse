e05ac9cf0f3f8fcaf3702a8679f9bc67903f47d0
danielbatres <danielbatresuser@gmail.com>
2022-06-25 12:19:57 -0600

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -38,3 +38,74 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 
 console.log(epicPhrase2);
+
+
+// LET y CONST, Multilínea, Spread Operator y Desestructuración
+
+// Multilínea
+
+// before es6
+
+let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n" + "otra frase epica que necesitamos";
+
+console.log(lorem);
+
+// after es6 
+
+let lorem2 = `Otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem2);
+
+// Desestructuración
+
+// before es6
+
+let person = {
+    name: "Daniel",
+    age: 16,
+    country: "SV",
+}
+
+console.log(person.name, person.age);
+
+// after es6
+
+let { name, age, country } = person;
+
+console.log(age, country);
+
+// Spread Operator ...
+
+let team1 = ["Daniel", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Jessica", "Camila"];
+
+let education = ["David", ...team1, ...team2]; // creamos un nuevo elemento donde tendremos a "David" con el team1 y team2 unidos.
+
+console.log(education);
+
+// Let y Const 
+
+// before es6
+
+var hola = "Hola"; // disponible de manera global
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = "Global Let";
+    console.log(globalLet); // si podemos acceder
+}
+
+console.log(globalVar);
+console.log(globalLet); // no podemos acceder
+
+// after es6
+
+let hola = "Hola"; // disponible de manera local
+
+const a = "b"; // "inmutable" disponible de manera local
+
+a = "a"; // error, no podemos reasignar un valor a const
